9e0d2eb89bd71b5a6819aa502e70903d72c10d4a
Rubtay <89306348+GolYeg@users.noreply.github.com>
2023-05-07 15:45:09 +0500

    Add files via upload
    right realization in cpp file. Previous was in hpp(

--- memory.cpp
+++ memory.cpp
@@ -1,5 +1,28 @@
 #include "memory.hpp"
 
+
+void Memory::mem_print() { 
+		for (int i = 0; i < 16; i++)
+			std::cout << regs_[i] << " "; 
+	}
+void Memory::negate_x () { x_reg_ = -x_reg_; }
+
+void Memory::reset_x () { x_reg_ = 0; }
+
+void Memory::swap_xy () { std::swap (x_reg_, y_reg_); }
+
+void Memory::set_x (arifmetic_type val) { x_reg_ = val; }
+
+void Memory::set_y (arifmetic_type val) { y_reg_ = val; }
+
+  
+void Memory::left_rotate () {
+		std::rotate (regs_.begin(), regs_.begin() + 1, regs_.end() - 9);}
+
+   
+void Memory::right_rotate () {
+    std::rotate (regs_.rbegin() - 9, regs_.rbegin() + 1, regs_.rend());}
+
 int main()
 {
 	Memory mem;
@@ -8,5 +31,4 @@ int main()
 	mem.mem_print();
 	return 0;
 }
-
 	
--- memory.hpp
+++ memory.hpp
@@ -10,37 +10,20 @@ class Memory final{
 
 		
 public:
-	void mem_print() { 
-		for (int i = 0; i < 16; i++)
-			std::cout << regs_[i] << " "; 
-	}
-
-	void negate_x () { x_reg_ = -x_reg_; }
-	void reset_x () { x_reg_ = 0; }
-
-   
-   	void swap_xy () { std::swap (x_reg_, y_reg_); }
+	void mem_print();
 	
-	void set_x (arifmetic_type val) { x_reg_ = val; }
-  	void set_y (arifmetic_type val) { y_reg_ = val; }
+	void negate_x ();
+	void reset_x ();
 
-  
-  	void left_rotate ()
-   
-   	{
-       
-		std::rotate (regs_.begin(), regs_.begin() + 1, regs_.end() - 9);
-   
-   	 }
+   	void swap_xy ();
+	
+	void set_x (arifmetic_type val);
+  	void set_y (arifmetic_type val);
 
+  	void left_rotate ();
+
+   	void right_rotate ();
    
-   	void right_rotate ()
-   
-        {
-       
-		std::rotate (regs_.rbegin() - 9, regs_.rbegin() + 1, regs_.rend());
-   
-        }
 };
 
 

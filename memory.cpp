df3bb497a5fc115974efb13161b4ae17b0547ba5
Rubtay <89306348+GolYeg@users.noreply.github.com>
2023-05-07 16:43:29 +0500

    Add files via upload
    with right_rotate func

--- memory.cpp
+++ memory.cpp
@@ -2,9 +2,15 @@
 
 
 void Memory::mem_print() { 
-		for (int i = 0; i < 16; i++)
-			std::cout << regs_[i] << " "; 
+	for (int i = 0; i < 16; i++)
+		std::cout << regs_[i] << " "; 
+	std::cout<<"\n";	
 	}
+void Memory::drum_print() {
+	for (int i = 0; i < 7; i++)
+			std::cout << regs_[i] << " ";	
+	std::cout <<"\n";
+}
 void Memory::negate_x () { x_reg_ = -x_reg_; }
 
 void Memory::reset_x () { x_reg_ = 0; }
@@ -15,20 +21,25 @@ void Memory::set_x (arifmetic_type val) { x_reg_ = val; }
 
 void Memory::set_y (arifmetic_type val) { y_reg_ = val; }
 
+void Memory::set_Pregs (int regnum, arifmetic_type val){
+	regnum+=6;
+    if ((regnum > 15) || (regnum < 8))
+		return;
+    regs_[regnum] = val;
+}
+Memory::arifmetic_type Memory::get_Pregs (int regnum){
+	regnum+=6;
+    if ((regnum > 15) || (regnum < 8))
+		return 0;
+    return regs_[regnum];	
+}
   
 void Memory::left_rotate () {
-		std::rotate (regs_.begin(), regs_.begin() + 1, regs_.end() - 9);}
+    std::rotate (regs_.begin(), regs_.begin() + 1, regs_.end() - 9);}
 
    
 void Memory::right_rotate () {
-    std::rotate (regs_.rbegin() - 9, regs_.rbegin() + 1, regs_.rend());}
+    std::rotate (regs_.rbegin() + 9, regs_.rbegin()+ 10, regs_.rend());}
 
-int main()
-{
-	Memory mem;
-	mem.mem_print();
-	mem.set_x(5);
-	mem.mem_print();
-	return 0;
-}
-	
+
+		
--- memory.hpp
+++ memory.hpp
@@ -1,16 +1,14 @@
-#include <iostream>
 #include <array>
 
 class Memory final{
-
 	using arifmetic_type =  double;
 	std::array<arifmetic_type, 16> regs_;
 	arifmetic_type &x_reg_ = regs_[0];
 	arifmetic_type &y_reg_ = regs_[7];
 
-		
 public:
 	void mem_print();
+	void drum_print();
 	
 	void negate_x ();
 	void reset_x ();
@@ -19,11 +17,12 @@ public:
 	
 	void set_x (arifmetic_type val);
   	void set_y (arifmetic_type val);
+	void set_Pregs (int regnum, arifmetic_type val);
+	
+	arifmetic_type get_Pregs (int regnum);
 
   	void left_rotate ();
-
    	void right_rotate ();
-   
 };
 
 

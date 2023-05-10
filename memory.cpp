3c2c1c022a783cb05f79a4b85fe017ece07162cb
Rubtay <89306348+GolYeg@users.noreply.github.com>
2023-05-10 19:56:24 +0500

    add P_flag, F_flag.

--- memory.cpp
+++ memory.cpp
@@ -1,14 +1,13 @@
 #include "memory.hpp"
 
-
-void Memory::mem_print() { 
+void Memory::mem_print(Memory& memory) { 
 	for (int i = 0; i < 16; i++)
-		std::cout << regs_[i] << " "; 
+		std::cout << memory.regs_[i] << " "; 
 	std::cout<<"\n";	
 	}
-void Memory::drum_print() {
+void Memory::drum_print(Memory& memory) {
 	for (int i = 0; i < 7; i++)
-			std::cout << regs_[i] << " ";	
+			std::cout << memory.regs_[i] << " ";	
 	std::cout <<"\n";
 }
 void Memory::negate_x () { x_reg_ = -x_reg_; }
@@ -18,8 +17,16 @@ void Memory::reset_x () { x_reg_ = 0; }
 void Memory::swap_xy () { std::swap (x_reg_, y_reg_); }
 
 void Memory::set_x (arifmetic_type val) { x_reg_ = val; }
+Memory::arifmetic_type Memory::get_x() { return x_reg_; }
 
 void Memory::set_y (arifmetic_type val) { y_reg_ = val; }
+Memory::arifmetic_type Memory::get_y() { return y_reg_; }
+
+void Memory::set_P_flag(bool val) { P_flag = val; }
+bool Memory::get_P_flag() { return P_flag; }
+
+void Memory::set_F_flag(bool val) { F_flag = val; }
+bool Memory::get_F_flag() { return F_flag; }
 
 void Memory::set_Pregs (int regnum, arifmetic_type val){
 	regnum+=6;
@@ -41,5 +48,8 @@ void Memory::left_rotate () {
 void Memory::right_rotate () {
     std::rotate (regs_.rbegin() + 9, regs_.rbegin()+ 10, regs_.rend());}
 
-
+int main()
+{
+	return 0;
+}
 		
--- memory.hpp
+++ memory.hpp
@@ -1,4 +1,8 @@
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
 #include <array>
+#include <iostream>
 
 class Memory final{
 	using arifmetic_type =  double;
@@ -6,23 +10,35 @@ class Memory final{
 	arifmetic_type &x_reg_ = regs_[0];
 	arifmetic_type &y_reg_ = regs_[7];
 
+	bool P_flag = 0;
+	bool F_flag = 0;
+
 public:
-	void mem_print();
-	void drum_print();
-	
 	void negate_x ();
 	void reset_x ();
 
    	void swap_xy ();
 	
 	void set_x (arifmetic_type val);
+	arifmetic_type get_x ();
+
   	void set_y (arifmetic_type val);
+	arifmetic_type get_y ();
+
+	void set_P_flag (bool val);
+	bool get_P_flag ();
+
+	void set_F_flag (bool val);
+	bool get_F_flag ();
+
 	void set_Pregs (int regnum, arifmetic_type val);
-	
 	arifmetic_type get_Pregs (int regnum);
 
   	void left_rotate ();
    	void right_rotate ();
+
+	static void mem_print(Memory& memory);
+	static void drum_print(Memory& memory);
 };
 
-
+#endif

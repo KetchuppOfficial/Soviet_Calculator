5cd9d4b4449acc0a53ea7c8c968b195aa0d6faac
Rubtay <89306348+GolYeg@users.noreply.github.com>
2023-05-12 15:56:37 +0500

    the last version of memory

--- memory.cpp
+++ memory.cpp
@@ -1,55 +1,100 @@
+#include <iostream>
 #include "memory.hpp"
 
-void Memory::mem_print(Memory& memory) { 
-	for (int i = 0; i < 16; i++)
-		std::cout << memory.regs_[i] << " "; 
-	std::cout<<"\n";	
-	}
-void Memory::drum_print(Memory& memory) {
-	for (int i = 0; i < 7; i++)
-			std::cout << memory.regs_[i] << " ";	
-	std::cout <<"\n";
+typename Memory::arifmetic_type Memory::operator[](std::size_t i) const
+{
+    return regs_[i];
 }
-void Memory::negate_x () { x_reg_ = -x_reg_; }
 
-void Memory::reset_x () { x_reg_ = 0; }
+void Memory::negate_x()
+{
+	 x_reg_ = -x_reg_;
+}
 
-void Memory::swap_xy () { std::swap (x_reg_, y_reg_); }
+void Memory::reset_x () 
+{ 
+	x_reg_ = 0; 
+}
 
-void Memory::set_x (arifmetic_type val) { x_reg_ = val; }
-Memory::arifmetic_type Memory::get_x() { return x_reg_; }
+void Memory::swap_xy () 
+{
+	std::swap (x_reg_, y_reg_); 
+}
 
-void Memory::set_y (arifmetic_type val) { y_reg_ = val; }
-Memory::arifmetic_type Memory::get_y() { return y_reg_; }
+void Memory::set_x (arifmetic_type val) 
+{ 
+	x_reg_ = val; 
+}
 
-void Memory::set_P_flag(bool val) { P_flag = val; }
-bool Memory::get_P_flag() { return P_flag; }
+typename Memory::arifmetic_type Memory::get_x () const
+{ 
+	return x_reg_; 
+}
 
-void Memory::set_F_flag(bool val) { F_flag = val; }
-bool Memory::get_F_flag() { return F_flag; }
+void Memory::set_y (arifmetic_type val) 
+{ 
+	y_reg_ = val; 
+}
 
-void Memory::set_Pregs (int regnum, arifmetic_type val){
+Memory::arifmetic_type Memory::get_y () const 
+{ 
+	return y_reg_; 
+}
+
+void Memory::set_P_flag (bool val) 
+{ 
+	P_flag_ = val; 
+}
+
+bool Memory::get_P_flag () const
+{ 
+	return P_flag_; 
+}
+
+void Memory::set_F_flag(bool val) 
+{ 
+	F_flag_ = val; 
+}
+
+bool Memory::get_F_flag() const 
+{ 
+	return F_flag_; 
+}
+
+void Memory::set_Pregs (std::size_t regnum, arifmetic_type val)
+{
 	regnum+=6;
     if ((regnum > 15) || (regnum < 8))
 		return;
     regs_[regnum] = val;
 }
-Memory::arifmetic_type Memory::get_Pregs (int regnum){
-	regnum+=6;
+
+Memory::arifmetic_type Memory::get_Pregs (std::size_t regnum) const
+{
+	regnum += 6;
     if ((regnum > 15) || (regnum < 8))
 		return 0;
     return regs_[regnum];	
 }
   
-void Memory::left_rotate () {
-    std::rotate (regs_.begin(), regs_.begin() + 1, regs_.end() - 9);}
-
-   
-void Memory::right_rotate () {
-    std::rotate (regs_.rbegin() + 9, regs_.rbegin()+ 10, regs_.rend());}
-
-int main()
+void Memory::left_rotate () 
 {
-	return 0;
+    std::rotate (regs_.begin(), regs_.begin() + 1, regs_.end() - 9);
+}  
+
+void Memory::right_rotate () 
+{
+    std::rotate (regs_.rbegin() + 9, regs_.rbegin() + 10, regs_.rend());
 }
-		
+
+size_t Memory::n_regs() const
+{
+    return regs_.size();
+}
+
+void mem_print(const Memory& memory)
+{
+	for (auto i = 0; i < memory.n_regs(); i++)
+		std::cout << memory[i] << " "; 
+	std::cout << '\n';	
+}
--- memory.hpp
+++ memory.hpp
@@ -2,43 +2,45 @@
 #define MEMORY_HPP
 
 #include <array>
-#include <iostream>
 
-class Memory final{
-	using arifmetic_type =  double;
+class Memory final
+{
+	using arifmetic_type = double;
 	std::array<arifmetic_type, 16> regs_;
 	arifmetic_type &x_reg_ = regs_[0];
 	arifmetic_type &y_reg_ = regs_[7];
 
-	bool P_flag = 0;
-	bool F_flag = 0;
+	bool P_flag_ = false;
+	bool F_flag_ = false;
 
 public:
+	arifmetic_type operator [] (std::size_t i) const;
 	void negate_x ();
 	void reset_x ();
 
    	void swap_xy ();
 	
 	void set_x (arifmetic_type val);
-	arifmetic_type get_x ();
+	arifmetic_type get_x () const;
 
   	void set_y (arifmetic_type val);
-	arifmetic_type get_y ();
+	arifmetic_type get_y () const;
 
 	void set_P_flag (bool val);
-	bool get_P_flag ();
+	bool get_P_flag () const;
 
 	void set_F_flag (bool val);
-	bool get_F_flag ();
+	bool get_F_flag () const;
 
-	void set_Pregs (int regnum, arifmetic_type val);
-	arifmetic_type get_Pregs (int regnum);
+	void set_Pregs (std::size_t regnum, arifmetic_type val);
+	arifmetic_type get_Pregs (std::size_t regnum) const;
 
   	void left_rotate ();
-   	void right_rotate ();
+   	void right_rotate ();	
 
-	static void mem_print(Memory& memory);
-	static void drum_print(Memory& memory);
+	size_t n_regs () const;
 };
+
+void mem_print (const Memory& memory);
 
 #endif

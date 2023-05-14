339ad0bf914311e6de2cc987673d7710c044afc9
Rubtay <89306348+GolYeg@users.noreply.github.com>
2023-05-14 20:17:51 +0500

    hpp without P and F flags. Added set_x_from_y

--- include/memory.hpp
+++ include/memory.hpp
@@ -3,9 +3,6 @@
 
 #include <array>
 
-namespace ussr
-{
-
 class Memory final
 {
 	using arifmetic_type = double;
@@ -13,14 +10,11 @@ class Memory final
 	arifmetic_type &x_reg_ = regs_[0];
 	arifmetic_type &y_reg_ = regs_[7];
 
-	bool P_flag_ = false;
-	bool F_flag_ = false;
-
 public:
 	arifmetic_type operator [] (std::size_t i) const;
 	void negate_x ();
 	void reset_x ();
-
+	void set_y_from_x();
    	void swap_xy ();
 	
 	void set_x (arifmetic_type val);
@@ -28,12 +22,6 @@ public:
 
   	void set_y (arifmetic_type val);
 	arifmetic_type get_y () const;
-
-	void set_P_flag (bool val);
-	bool get_P_flag () const;
-
-	void set_F_flag (bool val);
-	bool get_F_flag () const;
 
 	void set_Pregs (std::size_t regnum, arifmetic_type val);
 	arifmetic_type get_Pregs (std::size_t regnum) const;
@@ -45,7 +33,5 @@ public:
 };
 
 void mem_print (const Memory& memory);
-
-} // namespace ussr
 
 #endif

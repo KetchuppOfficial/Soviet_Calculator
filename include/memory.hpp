428e979270c29061f1dea7c6171a2c1c99df139d
KetchuppOfficial <dolgov04@list.ru>
2023-05-21 15:21:36 +0300

    Default initialization of memory added; size_t -> std::size_t

--- include/memory.hpp
+++ include/memory.hpp
@@ -9,7 +9,7 @@ namespace ussr
 class Memory final
 {
 	using arifmetic_type = double;
-	std::array<arifmetic_type, 16> regs_;
+	std::array<arifmetic_type, 16> regs_{};
 	arifmetic_type &x_reg_ = regs_[0];
 	arifmetic_type &y_reg_ = regs_[7];
 
@@ -33,7 +33,7 @@ public:
   	void left_rotate ();
    	void right_rotate ();	
 
-	size_t n_regs () const;
+	std::size_t n_regs () const;
 };
 
 void mem_print (const Memory& memory);
